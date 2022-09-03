e0d35fd33c09a736721a3f9f12c0567d7bf915f5
Hou Jun <2436906408@qq.com>
2022-09-03 17:18:42 +0800

    程序员第一次修改

---
+++ git_001_javaSE/src/main/java/com/hj/NewHello.java
@@ -0,0 +1,14 @@
+package com.hj;
+
+/**
+ * ClassName NewHello
+ * Author 24369
+ * Date 2022/9/3
+ * Version 17:10
+ * Status
+ */
+public class NewHello {
+    public static void main(String[] args) {
+        System.out.println("程序员的第一次修改");
+    }
+}
