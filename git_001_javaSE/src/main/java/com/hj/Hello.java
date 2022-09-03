180fb79294fed7e32caa39aab3d1c75e7931a395
Hou Jun <2436906408@qq.com>
2022-09-03 16:57:04 +0800

    项目经理完成了Hello类中的修改.

---
+++ git_001_javaSE/src/main/java/com/hj/Hello.java
@@ -0,0 +1,14 @@
+package com.hj;
+
+/**
+ * ClassName Hello
+ * Author 24369
+ * Date 2022/9/3
+ * Version 16:53
+ * Status
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("项目经理加入的内容");
+    }
+}
---
+++ git_002_javaWeb/src/main/java/com/hj/controller/One.java
@@ -0,0 +1,14 @@
+package com.hj.controller;
+
+/**
+ * ClassName One
+ * Author 24369
+ * Date 2022/9/3
+ * Version 16:54
+ * Status
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("项目经理新增的类");
+    }
+}
