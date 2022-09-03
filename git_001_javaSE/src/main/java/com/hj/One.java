85da874c16b7fc4d57d8ae50110601b5ac88911b
Hou Jun <2436906408@qq.com>
2022-09-03 17:37:13 +0800

    程序员准备拉分支

---
+++ git_001_javaSE/src/main/java/com/hj/One.java
@@ -0,0 +1,14 @@
+package com.hj;
+
+/**
+ * ClassName One
+ * Author 24369
+ * Date 2022/9/3
+ * Version 17:35
+ * Status
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("程序员准备拉分支");
+    }
+}
