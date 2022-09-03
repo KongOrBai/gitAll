5d94dfbdcd5a979e75371d5a7fd7a5ab9ce9a981
Hou Jun <2436906408@qq.com>
2022-09-03 15:42:52 +0800

    项目经理第一次将整个工程提交到本地仓库.

---
+++ git_002_javaWeb/src/main/java/com/hj/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.hj.controller;
+
+/**
+ * ClassName UserController
+ * Author 24369
+ * Date 2022/9/3
+ * Version 15:26
+ * Status
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("项目经理开发的web应用程序......");
+    }
+}
